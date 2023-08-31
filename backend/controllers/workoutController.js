6cdade3f1407929b3f4e92181b9e9ecde6d9eb66
Jammonjones <wjssaan@gmail.com>
2023-08-31 15:19:39 -0600

    backend done

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,12 +1,29 @@
 // imports
-    // model so the controller can interact with it and indirectly interact with the database
-        import workoutModel from "../models/workoutModel";
+    // npm modules
+        import mongoose from "mongoose";
+    // my modules
+            // model so the controller can interact with it and indirectly interact with the database
+                import workoutModel from "../models/workoutModel.js";
 
 // get all workouts
-
+    const getWorkouts = async (req, res) => {
+        const workouts = await workoutModel.find({}).sort({createdAt: -1});
+        res.status(200).json(workouts);
+    }
 
 // get a single workout
-
+    const getWorkout = async (req, res)=>{
+        const {id} = req.params;
+        // check if a valid mongoose id is passed in
+            if(!mongoose.Types.ObjectId.isValid(id)){
+                return res.status(404).json({error: "Enter a valid workout ID."});
+            }
+        const workout = await workoutModel.findById(id);
+        if(!workout){
+            return res.status(404).json({error: "No such workout"});
+        }
+        res.status(200).json(workout);
+    }
 
 // create a new workout
     const createWorkout = async (req, res)=>{
@@ -23,11 +40,33 @@
     }
 
 // delete a workout
-
+    const deleteWorkout = async(req, res)=>{
+        const {id} = req.params;
+        // check if a valid mongoose id is passed in
+            if(!mongoose.Types.ObjectId.isValid(id)){
+                return res.status(404).json({error: "Enter a valid workout ID."});
+            }
+        const workout = await workoutModel.findOneAndDelete({_id: id});
+        if(!workout){
+            return res.status(404).json({error: "No such workout"});
+        }
+        res.status(200).json(workout);
+    }
 
 // update a workout
-
+    const updateWorkout = async (req, res)=>{
+        const {id} = req.params;
+        // check if a valid mongoose id is passed in
+            if(!mongoose.Types.ObjectId.isValid(id)){
+                return res.status(404).json({error: "Enter a valid workout ID."});
+            }
+        const workout = await workoutModel.findOneAndUpdate({_id: id}, {...req.body})
+        if(!workout){
+            return res.status(404).json({error: "No such workout"});
+        }
+        res.status(200).json(workout)
+    }
 
 // exports
-export {createWorkout};
+export {createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout};
 
--- backend/routers/workoutRouter.js
+++ backend/routers/workoutRouter.js
@@ -2,32 +2,24 @@
     // npm modules
         import express from "express";
     // controller functions
-        import { createWorkout } from "../controllers/workoutController";
+        import { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout} from "../controllers/workoutController.js";
 
 // configurations
     const workoutRouter = express.Router();
 
 // get all workouts
-    workoutRouter.get("/", (req, res)=>{
-        res.json({mssg: "Get all workouts."})
-    })
+    workoutRouter.get("/", getWorkouts)
 
 // get a single workout
-    workoutRouter.get("/:id", (req, res)=>{
-        res.json({mssg: "Get a single workout"})
-    })
+    workoutRouter.get("/:id", getWorkout)
 
 // post a new workout
     workoutRouter.post('/', createWorkout)
 
 // delete a workout
-    workoutRouter.delete('/:id', (req, res) =>{
-        res.json({mssg: "Delete a workout"})
-    }) 
+    workoutRouter.delete('/:id', deleteWorkout) 
 
 // update a workout
-    workoutRouter.patch('/:id', (req, res) =>{
-        res.json({mssg: "Update a workout"})
-    })
+    workoutRouter.patch('/:id', updateWorkout)
 // exports
     export default workoutRouter;
